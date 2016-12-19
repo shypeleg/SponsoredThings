7a3d926cd3702e6645e788ffed8f4770c2e0bcb9
Gil Amran <gilamran@gmail.com>
2016-12-20 01:17:17 +0200

    using clickable marker

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,26 +1,53 @@
-function hideSponsoredParent(el) {
-	el.style.opacity = "0.3";
-	// el.style.display = "none";
-}
+(function () {
+	var options = {
+		useMarker: true
+	};
 
-function markSponsored(el) {
-	el.style.color = "#ea0000";
-	el.style.fontSize = "xx-large";
-}
+	function createMarker(onClick) {
+		var newDiv = document.createElement("div");
+		newDiv.style.backgroundColor = '#3b5998';
+		newDiv.style.padding = '10px';
+		newDiv.style.textAlign = 'center';
+		newDiv.style.color = 'white';
+		newDiv.style.cursor = "pointer";
+		newDiv.style.textDecoration = "underline";
 
-function investigateAddedElement(el) {
-	if (el.nodeType === 1) {
-		var sponsoredLink = el.querySelector('.uiStreamSponsoredLink');
-		if (sponsoredLink) {
-			markSponsored(sponsoredLink);
-			hideSponsoredParent(el);
-			console.log(`Sponsored: ${el}`);
+		var text = document.createTextNode("Sponsored Post - Removed");
+		newDiv.appendChild(text);
+
+		newDiv.onclick = onClick;
+		return newDiv;
+	}
+
+	function toggleVisibility(el) {
+		el.style.display !== 'none' ? el.style.display = 'none' : el.style.display = null;
+	}
+
+	function processSponsored(el) {
+		if (options.useMarker) {
+			var post = el.childNodes[0]; // yes, it's only one child
+			var marker = createMarker(() => toggleVisibility(post));
+			el.insertBefore(marker, post);
+			toggleVisibility(post);
+		} else {
+			el.style.opacity = "0.3";
 		}
 	}
-}
 
-var observer = new MutationObserver(mutations => {
-	mutations.forEach(mutation => mutation.addedNodes.forEach(investigateAddedElement));
-});
+	function investigateAddedElement(el) {
+		if (el.nodeType === 1) {
+			var sponsoredLink = el.querySelector('.uiStreamSponsoredLink');
+			if (sponsoredLink) {
+				processSponsored(el);
+				// console.log(el);
+			}
+		}
+	}
 
-observer.observe(document.getElementById('contentArea'), { subtree: true, childList: true });
+	var observer = new MutationObserver(mutations => {
+		mutations.forEach(mutation => mutation.addedNodes.forEach(investigateAddedElement));
+	});
+
+	observer.observe(document.getElementById('contentArea'), { subtree: true, childList: true });
+
+} ());
